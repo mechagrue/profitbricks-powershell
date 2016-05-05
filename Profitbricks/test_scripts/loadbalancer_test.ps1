c240057189de3bfe72aa76d37b9d880eaaa461f5
jasminSPC <jasmin@stackpointcloud.com>
2016-05-05 15:36:58 +0200

    load balancer tests completed

--- Profitbricks/test_scripts/loadbalancer_test.ps1
+++ Profitbricks/test_scripts/loadbalancer_test.ps1
@@ -8,7 +8,7 @@ $credentials = Get-Credential $Credentials
 
 Set-Profitbricks $credentials
 
-$newDc = New-PBDatacenter -Name "test_ps_1" -Description "PS Unit Testing" -Location "us/las"
+$newDc = New-PBDatacenter -Name "load balancer test" -Description "PS Unit Testing" -Location "us/las"
 
 $dcstatus = get-PBRequestStatus -RequestUrl $newDc.Request
 
@@ -18,66 +18,28 @@ $dcstatus.Metadata.MEssage
 
 $datacenter = Get-PBDatacenter $newDc.Id
 
-$newServer = New-PBServer -DataCenterId $datacenter.Id -Name "server_test" -Cores 1 -Ram 1024 
-
-Do{
-"Waiting on server to provision"
-
-$serverstatus = Get-PBRequestStatus -RequestUrl $newServer.Request
-
- start-sleep -seconds 5
-}While($serverstatus.Metadata.Status -ne "DONE")
-
-$newvolume = New-PBVolume -DataCenterId $datacenter.Id -Size 5 -Type HDD -ImageId 646023fb-f7bd-11e5-b7e8-52540005ab80 -Name "test_volume"
+$loadbalancer = New-PBLoadbalancer -DataCenterId $datacenter.Id -Name "test LB"
 
 
 Do{
-"Waiting on volume to provision"
-
-$volumestatus = Get-PBRequestStatus -RequestUrl $newvolume.Request
-
+"Waiting on load balancer to provision"
  start-sleep -seconds 5
-}While($volumestatus.Metadata.Status -ne "DONE")
 
-$newname = $newvolume.Properties.Name + "updated"
+$status = Get-PBRequestStatus -RequestUrl $loadbalancer.Request
 
-$updatedvolume = Set-PBVolume -DataCenterId $datacenter.Id -VolumeId $newvolume.Id -Name $newname
+}While($status.Metadata.Status -ne "DONE")
 
-start-sleep -seconds 10
 
-$volume = Get-PBVolume -DataCenterId $datacenter.Id -VolumeId $newvolume.Id
+$newname = $loadbalancer.Properties.Name + "updated"
 
-if($volume.Properties.Name -eq $newname){
-"Update - Check"
+$updated = Set-PBLoadbalancer -DataCenterId $datacenter.Id -LoadbalancerId $loadbalancer.Id -Name $newname
+
+if($updated.Properties.Name -eq $newname){
+"Update - check"
 }
-else
-{
-"Update failed"
+else{
+"Update - fail"
 }
-
-#attach the volume
-$attachedvolume = Attach-PBVolume -DataCenterId $datacenter.Id -ServerId $newServer.Id -VolumeId $volume.Id
-
-Do{
-"Waiting on volume to attach"
-
-$volumestatus = Get-PBRequestStatus -RequestUrl $attachedvolume.Request
-
- start-sleep -seconds 5
-}While($volumestatus.Metadata.Status -ne "DONE")
-
-$volumes = Get-PBAttachedVolume -DataCenterId $datacenter.Id -ServerId $newServer.Id
-
-if( $volumes.Item(0).Id -eq $attachedvolume.Id)
-{
-"Successfully attached to the server" 
-}
-else
-{
-"It failed"
-}
-
-Detach-PBVolume -DataCenterId $datacenter.Id -ServerId $newServer.Id -VolumeId $attachedvolume.Id
 
 Remove-PBDatacenter -DatacenterId $datacenter.Id
 
